658f74088956a6577bebb79b64109926faea8200
Jon Ruskin <jonabc@github.com>
2021-11-20 11:05:15 -0700

    reduce scope and code

--- cmd/job-cli/main.go
+++ cmd/job-cli/main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 	"sort"
 
-	"github.com/github/go-dbmigrator"
-	migratorCfg "github.com/github/go-dbmigrator/config"
-	"github.com/github/go-dbmigrator/mysql"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/jonabc/test-repo/gss/config"
-	"github.com/jonabc/test-repo/transitions"
 	"github.com/urfave/cli/v2"
 )
 
@@ -57,94 +50,11 @@ func realMain() error {
 // EnterpriseAction runs migrations and transitions together, for use in GHE environments.
 func EnterpriseAction(c *cli.Context) error {
 	// Your application will likely have its own configuration, app context, and database helper code to plug in here.
-	cfg, err := config.Load()
-	if err != nil {
-		return fmt.Errorf("loading configuration: %v", err)
-	}
-
-	dbCfg, err := cfg.NewDatabaseConfig()
-	if err != nil {
-		return fmt.Errorf("loading DB config: %v", err)
-	}
-
-	db, err := sql.Open("mysql", dbCfg.FormatDSN())
-	if err != nil {
-		return fmt.Errorf("opening sql DB: %v", err)
-	}
-	defer db.Close()
-
-	if err := db.Ping(); err != nil {
-		return fmt.Errorf("pinging DB: %v", err)
-	}
-
-	transitions, err := GetEnterpriseTransitions(db)
-	if err != nil {
-		return fmt.Errorf("getting enterprise transitions: %v", err)
-	}
-
-	root, err := migratorCfg.RootDir()
-	if err != nil {
-		return fmt.Errorf("getting root directory: %v", err)
-	}
-
-	fullSourceURL := fmt.Sprintf("file://%s", filepath.Join(root, "migrations"))
-
-	// TODO: construct migrator options with logger and pass it in
-	migrator, err := mysql.NewWithDatabaseInstance(nil, db, fullSourceURL, "test_migrations")
-	if err != nil {
-		return fmt.Errorf("creating migrator: %v", err)
-	}
-	defer migrator.Close()
-
-	return migrator.Migrate(c.Context, transitions)
+	return nil
 }
 
 // CloudAction runs the data transitions only, for use in cloud environments (locally and with Moda jobs).
 func CloudAction(c *cli.Context) error {
-	// Your application will likely have its own configuration, app context, and database helper code to plug in here.
-	cfg, err := config.Load()
-	if err != nil {
-		return fmt.Errorf("loading configuration: %v", err)
-	}
-
-	dbCfg, err := cfg.NewDatabaseConfig()
-	if err != nil {
-		return fmt.Errorf("loading database config: %v", err)
-	}
-
-	db, err := sql.Open("mysql", dbCfg.FormatDSN())
-	if err != nil {
-		return fmt.Errorf("opening sql DB: %v", err)
-	}
-	if err := db.Ping(); err != nil {
-		return fmt.Errorf("pinging DB: %v", err)
-	}
-
-	transitions, err := GetCloudTransitions(db)
-	if err != nil {
-		return fmt.Errorf("getting cloud transitions: %v", err)
-	}
-	trans := transitions.LatestTransition()
-	return trans.Run(c.Context)
-}
-
-// GetCloudTransitions returns all transitions to be applied for the database
-func GetCloudTransitions(db *sql.DB) (*dbmigrator.Transitioner, error) {
-	trans := dbmigrator.NewTransitioner()
-	err := trans.Add(20200604095405, transitions.Get20200604095405(db))
-	if err != nil {
-		return nil, err
-	}
-
-	return trans, nil
-}
-
-// GetEnterpriseTransitions returns all transitions to be applied in GHE
-func GetEnterpriseTransitions(db *sql.DB) (*dbmigrator.Transitioner, error) {
-	trans := dbmigrator.NewTransitioner()
-	err := trans.Add(20200604095405, transitions.Get20200604095405(db))
-	if err != nil {
-		return nil, err
-	}
-	return trans, nil
+	// Your application will likely have its own configuration, app context, and database helper code to plug in here
+	return nil
 }
